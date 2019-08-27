f6f5e76575cc9c2f85d707f2a3870c108de8ea4c
Huiwone <hannahkhw5@gmail.com>
2019-08-27 19:29:06 +0900

    hw

--- index.php
+++ index.php
@@ -1,4 +1,103 @@
 <?php
-echo "dddd";
-echo "안녕ㅎ";
+<!DOCTYPE html>
+<html lang="ko">
+<head>
+	 <meta charset="UTF-8">
+    <title>김희원 지원서</title>
+</head>
+<body bgcolor="#FFCCCC">
+ <div style="text-align: center">
+    <h1><b><font size="20", color="#FF0000", face="궁서체">닷</font><font size="20", color="#FF6600", face="궁서체">넷</font> <font size="20", color="#FFFF00", face="궁서체">지</font><font size="20", color="#009966", face="궁서체">원</font><font size="20", color="#330066", face="궁서체">서</font></b></h1>
+ </div>
+    
+    <h2>숙지 사항</h2>
+    <ul>
+        <li>자유롭게 HTML을 꾸며주세요.</li>
+        <li>유니코드를 준수해주세요.</li>
+        <li>코드를 깔끔하고 일관적이도록 유지해주세요.</li>
+    </ul>
+    
+    <h2><b>인적 사항</b></h2>
+    <table>
+        <tbody>
+	<br/>
+	 <img src="memememe.jpg" alt="원래 사진이 있었는데 오류가 떴네요ㅠ" width="300" />
+	<br/>
+            <tr>
+                <td>이름</td>
+                <td><i><ins>김희원</ins></i></td>
+            </tr>
+	<a href="https://www.facebook.com/profile.php?id=100017345052565">제 페이스북 프로필 링크입니다!!!!</a>
+            <tr>
+                <td>기수</td>
+                <td><ins>23기</ins></td>
+            </tr>
+            <tr>
+                <td>학번</td>
+                <td><ins>181039</ins></td>
+            </tr>
+            <tr>
+                <td>Gmail</td>
+                <td><ins>hannahkhw5@gmail.com</ins></td>
+            </tr>
+	<tr>
+	 <td>시간표 <del>(TMIㅎㅎ)</del></td>
+	<td>
+	 <table border="2">
+	  <thead>
+	   <tr>
+	   <td></td><td>월</td> <td>화</td> <td>수</td> <td>목</td> <td>금</td>
+	   </tr>
+ 	  </thead>
+	  <tbody>
+	   <tr>
+	    <td>1교시</td><td><font color="#CC66FF">애국조회</font></td><td><font color="#330066">물리C</font></td><td><font color="#330066">미적분</font></td><td><font color="#330066">영어2</font></td><td><font color="#330066">물리C</font></td>
+	   </tr>
+	   <tr>
+	    <td>2교시</td><td><font color="#FF0000">국어문법</font></td><td><font color="#330066">물리C</font></td><td><font color="#330066">화학2</font></td><td><font color="#330066">미적분</font></td><td><font color="#330066">물리C</font></td>
+	   </tr>
+	   <tr>
+	    <td>3교시</td><td><font color="#FF0000">국어문법</font></td><td><font color="#009966">한국사</font></td><td>융합프로젝트</td><td><font color="#009966">댄스스포츠</font></td><td><font color="#330066">선형대수</font></td>
+	   </tr>
+	   <tr>
+	    <td>4교시</td><td><font color="#330066">미적분</font></td><td>공강!</td><td>융합프로젝트</td><td><font color="#009966">댄스스포츠</font></td><td><font color="#FF0000">국어문법</font></td>
+	   </tr>
+	   <tr>
+	    <td>5교시</td><td><font color="#330066">미시경제</font></td><td><font color="#FF0000">가야금</font></td><td><font color="#330066">영어2</font></td><td><font color="#330066">화학2</font></td><td><font color="#009966">한국사</font></td>
+	   </tr>
+	   <tr>
+	    <td>6교시</td><td><font color="#330066">미시경제</font></td><td><font color="#FF0000">가야금</font></td><td><font color="#330066">선형대수</font></td><td><font color="#330066">화학2</font></td><td><font color="330066">상담</font></td>
+	   </tr>
+	   <tr>
+	    <td>7교시</td><td><font color="#330066">선형대수</font></td><td><font color="#330066">미시경제</font></td><td>공강!</td><td>공강!</td><td>융합프로젝트</td>
+	   </tr>
+	   <tr>
+	    <td>8교시</td><td><font color="#330066">영어2</font></td><td>공강!</td><td>공강!</td><td>공강!</td><td><font color="#330066">미시경제</font></td>
+	   </tr>
+	  </tbody>
+	 </table>
+	<font color="#FF0000">빨간색은 민교관, </font><font color="#009966">초록색은 충무관, </font><font color="#330066">파란색은 다산관, </font> <font color="#CC66FF"> 보라색은 체육관</font>
+	</td>
+	</tr>
+        </tbody>
+   </table>
+
+	<hr />
+    
+    <h2>닷넷에 지원해주신 이유는 무엇입니까?</h2>
+    <p>고등학교에 와서 처음 컴퓨터에 대해서 배우게 되었고 흥미가 생겼습니다. <br>저는 컴퓨터를 더 배우고 싶었고 제가 배운 것들을 응용하여 (특히 KmlaOnline을 통해서) 남들에게 도움도 주고 싶어서 닷넷에 지원했습니다.<br> 저는 지난 1년 반동안 KmlaOnline을 사용하면서 정말 편하게 정보를 얻을 수 있었지만 닷넷에 들어가서 더 편한 방법이 있다면 그 개선 과정에 참여하고 싶습니다!</p>
+    
+    <h2 >당신은 얼마나 컴퓨터와 친한가요? (학교 수업, 개인적인 경험, 등으로 접한 사례)</h2>
+    <p>
+	<ol>
+		<li> 학교 수업: 고등학교 1학년 때 필수로 정보 수업 총 4단위를 듣고, 2학년 1학기 때는 프로그래밍 과목을 수강하였습니다.</li>
+		<li> 여름 학기 때 수강했던 기초 선형 대수 과목에서 제출해야 하는 자율 과제 보고서에서 <br />자바로 행렬의 간단한 연산을 하는 프로그램을 만들어서 제출한 경험도 있습니다. </li>
+		<li> 개인적인 경험: 더 배우고자 서울대학교 분산시스템 연구실에 견학을 가본 적이 있습니다. (잘 이해는 하지 못했습니다.) </li>
+		<li> 위에 언급한 것들 외에도 책을 사서 스스로 컴퓨터 공부도 해보았고, <del>노트북에 깔려 있는 것(프로그램)들을 자주 구경합니다. </del></li>
+	</ol>
+</p>
+    
+    <div><pre class="line-numbers"><code class="language-javascript">console.log(&quot;Good Luck!&quot;);</code></pre></div>
+</body>
+</html>
 ?>

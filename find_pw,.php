141d8d52020f3d564faadccea422fe81b035f77b
조성윤 <yoon@joseong-yun-ui-MacBookAir.local>
2019-09-01 20:08:00 +0900

    login004

--- find_pw,.php
+++ find_pw,.php
@@ -2,18 +2,26 @@
     
     require('db.php');
 
-    $name = $_POST['name'];
-    $id = $_POST['id'];
+    $name_input = $_POST['name'];
+    $id_input = $_POST['id'];
     
-    $check_name="SELECT *from user_info WHERE username='$name'";
-    $check_id="SELECT *from user_info WHERE userid='$id'";
-
-    $result_name=$mysqli->query($check_name);
-    $result_id=$mysqli->query($check_name);
-
-    if($result_name->num_rows==1 && $result_id->num_rows==1)
+    $check_name="SELECT *from user_info WHERE username='$name_input'";
+    $check_id="SELECT *from user_info WHERE userid='$id_input'";
+    $result=$mysqli->query($check);
+    if($result->num_rows==1) //result안에 있는 
     {
-        echo "pw";
-        exit();
+        
     }
+
+    $signup=mysqli_query($mysqli,"INSERT INTO user_info (id,name,pw) VALUES ('$id','$name','$pw')");
+    if($signup)
+    {
+        ?>
+        <meta charset="utf-8" />
+        <script type="text/javascript">alert('회원가입이 완료되었습니다.');</script>
+        <meta http-equiv="refresh" content="0 url=/">
+        <?php
+    }
+    else
+        echo "<button onclick=\"location.href='signup.html'\"> 회원가입 실패, 돌아가기 </button>";
 ?>

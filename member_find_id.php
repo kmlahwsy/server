1aa6ab02db6b7cad67111ab40e4c75cede9ca8d9
HuiwoneKim1024 <hannahkh5@gmail.com>
2019-09-07 14:19:55 +0900

    dsjlkfsajlk

---
+++ member_find_id.php
@@ -0,0 +1,19 @@
+<?php
+include "../db.php";
+if($_POST["name"] == "" || $_POST["st_id"] == ""){
+		echo '<script> alert("항목을 입력하세요"); history.back(); </script>';
+	}else{
+
+	$name = $_POST['name'];
+	$st_id = $_POST['st_id'];
+
+$sql = mq("select * from member where name = '{$name}' && st_id = '{$st_id}'");
+$result = $sql->fetch_array();
+
+if($result["name"] == $name){
+	echo "<script>alert('회원님의 ID는 ".$result['id']."입니다.'); history.back();</script>";
+}else{
+echo "<script>alert('없는 계정입니다.'); history.back();</script>";
+}
+}
+?>

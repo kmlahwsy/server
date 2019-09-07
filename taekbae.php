6575d95b942ad81e46fdac485f05de5db6e7afac
HuiwoneKim1024 <hannahkh5@gmail.com>
2019-09-07 15:06:26 +0900

    sdfaklj

--- taekbae.php
+++ taekbae.php
@@ -1,12 +1,13 @@
 <?php
   $t_name = $_GET['t_name'];
+  echo "<p>오늘 택배 나온 사람: $st_id $name($id)</p>";
 ?>
 
 <br/>
 <a href="taekbae.html">
 	<input type="button" name="t_continue" value="입력계속">
 </a>
-<a href="find_pw.php">
+<a href="index.php">
 	<input type="button" name="t_end" value="입력완료">
 </a>
 </form>

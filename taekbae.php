ea31a2b49d4a6129a91687ee41522f89ab9f5566
HuiwoneKim1024 <hannahkh5@gmail.com>
2019-09-07 18:46:22 +0900

    sjldlsljkdlkdsf

--- taekbae.php
+++ taekbae.php
@@ -1,7 +1,17 @@
 <?php
   require('db_t.php');
   $t_name = $_GET['t_name'];
-  echo "<p>오늘 택배 나온 사람: $t_name </p>";
+  $t_no = $_GET['t_no'];
+
+  $tt=mysqli_query($mysqli,"INSERT INTO t_list (t_name,t_no) VALUES ('$t_name','$t_no')");
+    if($tt)
+    {
+        ?>
+        <meta charset="utf-8" />
+        <meta http-equiv="refresh" content="0 url=/">
+        <?php
+        echo "<p>오늘 택배 나온 사람: $t_name </p>";
+    }
 ?>
 
 <br/>

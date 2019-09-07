471fe2706222e02663eaa66701ca6afc5ee58588
조성윤 <yoon@joseong-yun-ui-MacBookAir.local>
2019-09-08 01:24:39 +0900

    taekbae0001

--- taekbae.php
+++ taekbae.php
@@ -4,6 +4,16 @@
   $t_no = $_GET['t_no'];
 
   $sql_t=mysqli_query($mysqli,"INSERT INTO t_list (name,no) VALUES ('$t_name','$t_no')");
+  if($signup)
+    {
+        ?>
+        <meta charset="utf-8" />
+        <script type="text/javascript">alert('추가 완료!');</script>
+        <meta http-equiv="refresh" content="0 url=/">
+        <?php
+    }
+    else
+        echo "<button onclick=\"location.href='signup.html'\"> 추가 실패, 다시 시도해주세요. </button>";
   echo "<p>오늘 택배 나온 사람: $t_name (번호 $t_no) </p>";
 ?>
 

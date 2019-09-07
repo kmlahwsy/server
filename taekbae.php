ce7d76f4289fce1c2fdb1f181669d821df7202ab
HuiwoneKim1024 <hannahkh5@gmail.com>
2019-09-07 15:00:56 +0900

    sdaf

---
+++ taekbae.php
@@ -0,0 +1,12 @@
+<?php
+  $t_name = $_GET['t_name'];
+?>
+
+<br/>
+<a href="taekbae.html">
+	<input type="button" name="t_continue" value="입력계속">
+</a>
+<a href="find_pw.php">
+	<input type="button" name="t_end" value="입력완료">
+</a>
+</form>

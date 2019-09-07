c05fb8e3b01f6a0d2442b1cce295edb1f31373f0
조성윤 <yoon@homehub.olleh.com>
2019-09-07 14:19:46 +0900

    login006

--- signup_check.php
+++ signup_check.php
@@ -10,7 +10,6 @@
     const PASSWORD_COST = ['cost'=>12];// cost 의 기본 값은 10,시간이 조금 더 걸려도 좀 더 강력하게 보안 가능하게 cost 값 10으로
     /* 암호화 */
     $hash = password_hash($pw , PASSWORD_DEFAULT, PASSWORD_COST);
- 
     /* 패스워드 검증 */
     if ( !password_verify($password ,$hash) ) {
         throw new Exception('비밀번호가 일치하지 않습니다.');

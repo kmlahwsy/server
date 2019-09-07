d6fb9769d34d3320dd1ad7941b66264c02fafc48
HuiwoneKim1024 <hannahkh5@gmail.com>
2019-09-07 19:06:52 +0900

    sdklsdfl

--- taekbae.php
+++ taekbae.php
@@ -1,9 +1,9 @@
 <?php
-  mysqli_connect(us-cdbr-iron-east-02.cleardb.net,b0166a7ca9fa10,b0166a7ca9fa10,t_list);
+  $con=mysqli_connect("us-cdbr-iron-east-02.cleardb.net","b0166a7ca9fa10","88d08067","t_list");
   $t_name = $_GET['t_name'];
   $t_no = $_GET['t_no'];
 
-  "INSERT INTO t_list (name,no) VALUES ('$t_name','$t_no')";
+  $sql_t="INSERT INTO t_list (name,no) VALUES ('$t_name','$t_no')";
   echo "<p>오늘 택배 나온 사람: $t_name (번호 $t_no) </p>";
 ?>
 

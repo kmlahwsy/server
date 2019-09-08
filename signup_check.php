e998e2323d06f6ff119a4d1f4683db9a8a246cc8
조성윤 <yoon@joseong-yun-ui-MacBookAir.local>
2019-09-08 20:04:21 +0900

    signup002

--- signup_check.php
+++ signup_check.php
@@ -6,8 +6,8 @@
     $name = $_GET['name'];
     $password = $_POST['pw'];
     $st_id = $_POST['st_id'];
+
     
-    /*
     const PASSWORD_COST = ['cost'=>12];// cost 의 기본 값은 10,시간이 조금 더 걸려도 좀 더 강력하게 보안 가능하게 cost 값 10으로
     // 암호화
     $pw = password_hash($password , PASSWORD_DEFAULT, PASSWORD_COST);
@@ -15,7 +15,7 @@
     if ( !password_verify($password ,$pw) ) {
         throw new Exception('비밀번호가 일치하지 않습니다.');
     }
-    */
+    
 
 
     $check="SELECT *from user_info WHERE id='$id'";

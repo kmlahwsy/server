972444503f966d8ed82d22e756f3cbc0ebb902a8
HuiwoneKim1024 <hannahkh5@gmail.com>
2019-09-08 19:32:54 +0900

    sdljfds

--- taekbae.php
+++ taekbae.php
@@ -1,22 +1,29 @@
 <?php
-  $t_name = $_GET['t_name'];
-  $t_no = $_GET['t_no'];
-
   $con_t=mysqli_connect("us-cdbr-iron-east-02.cleardb.net", "b0166a7ca9fa10", "88d08067", "t_list");
-  $sql_t="INSERT INTO t_list (name,no)";
-  $sql_t .= "VALUES ('$t_name','$t_no')";
+  $sql_t .= ;
   mysqli_query($con_t, $sql_t);
   mysqli_close($con_t);
-  if($sql_t)
+  if($con_t)
     {
-        ?>
-        <meta charset="utf-8" />
-        <script type="text/javascript">alert('추가 완료!');</script>
-        <?php
+      $t_name = $_GET['t_name'];
+      $t_no = $_GET['t_no'];
+      $sql_t="INSERT INTO t_list (name,no) VALUES ('$t_name','$t_no')";
+      mysql_query($sql_t,$con_t);
+      echo "추가완료!";
     }
     else
-        echo "<button onclick=\"location.href='taekbae.html'\"> 추가 실패, 다시 시도해주세요. </button>";
-  echo "<p>오늘 택배 나온 사람: $t_name (번호 $t_no) </p>";
+    {
+     echo "<button onclick=\"location.href='taekbae.html'\"> 추가 실패, 다시 시도해주세요. </button>";
+    }
+
+    $query= "select * from t_list where name = '{$t_name}' && no = '{$t_no}'";
+    $result= mysql_query($query,$con_t);
+    while ($data = mysql_fetch_array($result))
+    {
+      $tname = $data['name'];
+      $tno = $daya['no'];
+      echo "<p>오늘 택배 나온 사람: $tname (번호 $tno) </p>";
+    }
 ?>
 
 <br/>

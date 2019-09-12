79ae568520c04d3b10cf3561a260c980cda6f7f8
HuiwoneKim1024 <hannahkh5@gmail.com>
2019-09-12 23:46:27 +0900

    change

--- index.php
+++ index.php
@@ -22,24 +22,30 @@
 
 			?>
 
+			<?php //택배 리스트 입력하는 버튼 (이름과 택배 번호) --> taekbae.html와 연결 ?>
 			<a href="taekbae.html">
 				<input type="button" name="taekbaebutton" value="택배 리스트 입력하러 가기">
 			</a>
 			<br />
+
+			<?php //빨래 리스트 입력하는 버튼 (학번) --> bballae.html와 연결 ?>
 			<a href="bballae.html">
 				<input type="button" name="bballaebutton" value="빨래통 리스트 입력하러 가기">
 			</a>
 			
 			<?php
+				//로그아웃하는 버튼 --> logout.php와 연결
 				echo "<p><a href='logout.php'>로그아웃</a></p>"
 			?>
-
 			<br />
+
+			<?php //회원가입하러 가는 버튼 --> signup.html와 연결 ?>
         	<a href="signup.html">
 				<input type="button" name="signupbtn" value="회원가입 하러가기">
 			</a>
 			<br />
 			
+			<?php //비밀번호 찾으러 가는 버튼 --> find_pw.php와 연결 ?>
 			<a href="find_pw.php">
 				<input type="button" name="findpwbtn" value="비밀번호 찾으러가기">
 			</a>

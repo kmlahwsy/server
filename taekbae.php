bfa5ee7e1c848b5ccfadd1d8e62f59c660216f04
Huiwone <hannahkhw5@gmail.com>
2019-09-30 14:17:37 +0900

    tt

--- taekbae.php
+++ taekbae.php
@@ -1,14 +1,14 @@
 <?php
  // $con_t=mysqli_connect("us-cdbr-iron-east-02.cleardb.net", "b0166a7ca9fa10", "88d08067", "t_list");
   
-
+    //t_list 접속
     $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
     $server = $url["host"];       
     //$host = 'us-cdbr-iron-east-02.cleardb.net';
     $user = 'b0166a7ca9fa10';
     $pw = '88d08067';
     $dbName = 't_list';
-    $mysqli_t = new mysqli($server, $user, $pw, $dbName);
+    $mysqli_t = mysqli_connect($server, $user, $pw, $dbName);
 
     if ($mysqli_t){
       echo "MySQL 접속 성공";

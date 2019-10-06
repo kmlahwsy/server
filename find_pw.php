b7b13b0c6585dc0a71eb0d8adf3314f11dbb4b94
jueon an <aje51488815@gmail.com>
2019-10-06 21:44:40 +0900

    오타;;

--- find_pw.php
+++ find_pw.php
@@ -9,32 +9,32 @@
 <!DOCTYPE html>
 <html lang=ko>
 
-<head>
-<meta charset="utf-8">
-<title>비밀번호 찾기<title>
-  <!--
-<style>
-* {margin: 0 auto;}
-a {color:#333; text-decoration: none;}
-.find {text-align:center; width:500px; margin-top:30px; }
-</style>
--->
-</head>
+      <head>
+      <meta charset="utf-8">
+      <title>비밀번호 찾기</title>
+        <!--
+      <style>
+      * {margin: 0 auto;}
+      a {color:#333; text-decoration: none;}
+      .find {text-align:center; width:500px; margin-top:30px; }
+      </style>
+      -->
+      </head>
 
-<body>
-  <form method="post" action="member_find_pw.php">
+      <body>
+        <form method="post" action="member_find_pw.php">
 
-        비밀번호 찾기
-            <table>
-              <tr>
-                <td>아이디</td>
-                <td><input type="text" size="35" name="userid" placeholder="아이디"></td>
-              </tr>
-            </table>
-          <input type="submit" value="비밀번호 찾기" />
+              비밀번호 찾기
+                  <table>
+                    <tr>
+                      <td>아이디</td>
+                      <td><input type="text" size="35" name="userid" placeholder="아이디"></td>
+                    </tr>
+                  </table>
+                <input type="submit" value="비밀번호 찾기" />
 
-    </form>
-  </div>
-</body>
+          </form>
+        </div>
+      </body>
 </html>
 

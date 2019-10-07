410a04eccd38553cb9215b603ce02edb54b51180
Huiwone <hannahkhw5@gmail.com>
2019-10-07 11:12:49 +0900

    빨래통 초기화

--- bballae_reset.php
+++ bballae_reset.php
@@ -1,7 +1,11 @@
 <?php 
     require('db.php');
-    $delete = "DELETE * FROM b_list";
+    $delete = mysqli_query($mysqli,"DELETE * FROM b_list");
     if ($delete) {
         echo "초기화 완료!";
     }
 ?>
+<br />
+<a href="bballae.html">
+	<input type="button" name="b_continue" value="입력계속">
+</a>

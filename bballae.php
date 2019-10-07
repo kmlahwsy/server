5a453877e8e50c214480cabe6929da5c363fa102
Huiwone <hannahkhw5@gmail.com>
2019-10-07 10:43:48 +0900

    빨래 수정

--- bballae.php
+++ bballae.php
@@ -15,12 +15,12 @@
 
   echo "<p>오늘 빨래통 나온 사람들: </p>";
   $check_b="SELECT * FROM b_list";
-  $check_u="SELECT * FROM user_info";
-  $result_b=$mysqli->query($check_b); 
-  $result_u=$mysqli->query($check_u);
+  $result_b=$mysqli->query($check_b);
   while($newrow_b = mysqli_fetch_array($result_b))
     {
         $b_stid = $newrow_b['st_id'];
+        $check_u="SELECT * FROM user_info WHERE st_id=b_stid";
+        $result_u=$mysqli->query($check_u);
         $newrow_u = mysqli_fetch_array($result_u);
         $b_name = $newrow_u['name'];
         echo "<p> $b_name ($b_stid) <p>";

41229a7da15b70893e1933f9bd0df053dde740b3
Huiwone <hannahkhw5@gmail.com>
2019-10-08 23:50:37 +0900

    됏으면 좋겠다

--- index.php
+++ index.php
@@ -8,6 +8,7 @@
     <body>
             <h2>빨래통, 택배 찾기!!<h2>
             <?php
+				require('db.php');
 				session_start();
 
 				if(!isset($_SESSION['id']) || !isset($_SESSION['name']))
@@ -16,7 +17,7 @@
 					exit;
 				}
 				$id = $_SESSION['id'];
-				$name = $_SESSION['name'];
+				//$name = $_SESSION['name'];
 				
 				
 				/*
@@ -32,11 +33,23 @@
 				return html_entity_decode(preg_replace("/%u([0-9a-f]{3,4})/i","&#x\\1;",urldecode($string)), null, 'UTF-8');
 				}*/
 
-				error_log("hello, this is a test!");
+				//error_log("hello, this is a test!");
 
-				$name_u = htmlentities($name);
+				$check="SELECT * FROM user_info WHERE id='$id'";
+    			$result=$mysqli->query($check); 
+				
+				//이름이 한글로 안뜨길래 id를 이용해서 db에서 이름을 가져옴
+   				if($result->num_rows==1)
+    			{
+        			//id에 correspond하는 하나의 열을 배열로 가져오기
+					$row=$result->fetch_array(MYSQLI_ASSOC);
+					$name = $row['name'];
+					$_SESSION['name']=$name;
+				}
 
-				echo "<p>안녕하세요. $name_u ($id) 님</p>";
+				//$name_u = htmlentities($name);
+
+				echo "<p>안녕하세요. $name ($id) 님</p>";
 
 			//택배 리스트 입력하는 버튼 (이름과 택배 번호) --> taekbae.html와 연결 ?>
 			<a href="taekbae_admin_check.html">

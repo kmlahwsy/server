3fb2dd232e3e66a36da2f8589ac67b309d6c456f
Huiwone <hannahkhw5@gmail.com>
2019-10-10 19:40:19 +0900

    택배리스트 수정

--- taekbae_list.php
+++ taekbae_list.php
@@ -12,12 +12,9 @@
      $result_t=$mysqli->query($check_t);
      while($newrow_t = mysqli_fetch_array($result_t))
          {
-             $t_stid = $newrow_t['st_id'];
-             $check_u="SELECT * FROM user_info WHERE st_id=$t_stid";
-             $result_u=$mysqli->query($check_u);
-             $newrow_u = mysqli_fetch_array($result_u);
-             $t_name = $newrow_u['name'];
-             echo "<p> $t_name ($t_stid) <p>";
+             $t_name = $newrow_t['name'];
+             $t_no = $newrow_u['n0'];
+             echo "<p> $t_name ($t_no) <p>";
              echo "<br>";
          }
 ?>
